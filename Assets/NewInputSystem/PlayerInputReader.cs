df6136ed635f5f67d98d238b8dbdd5a2134ead13
Aeon Amberwood <aeon.amberwood@icloud.com>
2026-07-28 22:56:49 +1000

    Comments

--- Assets/NewInputSystem/PlayerInputReader.cs
+++ Assets/NewInputSystem/PlayerInputReader.cs
@@ -3,31 +3,64 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputReader : MonoBehaviour
 {
-    public Vector2 direction;
+    /// <summary>
+    /// The input direction from the player, e.g. arrow keys
+    /// </summary>
+    public Vector2 direction { get; private set; }
     
-    public bool jumpPressed;
-    public bool jumpHeld;
+    /// <summary>
+    /// Whether jump was pressed this frame
+    /// </summary>
+    public bool jumpPressed{ get; private set; }
 
+    /// <summary>
+    /// Whether jump is currently held
+    /// </summary>
+    public bool jumpHeld { get; private set; }
+
+    // LateUpdate() runs after everything has done Updare
     void LateUpdate()
     {
         jumpPressed = false;
     }
 
+    #region BroadcastMessages
+    // Methods must be named 'OnActionName', e.g. 'OnJump', 'OnSprint'
+    // Methods may have an InputValue parameter for more than just "this was pressed"
+    // BroadcastMessages messages will fire when buttons are 
     public void OnMove(InputValue inputValue)
     {
+        // .Get<T>() gets the contained value from the input
         direction = inputValue.Get<Vector2>();
     }
 
     public void OnJump(InputValue inputValue)
     {
+        // InputValue.isPressed will be true if the button has just been pressed
         jumpPressed = inputValue.isPressed;
         jumpHeld = inputValue.isPressed;
-        print("jump is pressed");
     }
 
+    #endregion
+
+    #region UnityEvent
+    // Methods can be named anything
+    // Methods may have an InputAction.CallbackContext parameter for specifics about the action
+    // UnityEvent messages will fire anytime the input value changes, from press to release
     public void UpdateJumpValue(InputAction.CallbackContext callbackContext)
     {
+        // CallbackContext contains different 'phases' we can check
+        // .performed means the button was just pressed
         jumpPressed = callbackContext.performed;
+
+        // .cancelled means the button is no longer pressed
         jumpHeld = callbackContext.performed || !callbackContext.canceled;
     }
+
+    public void SetDirection(InputAction.CallbackContext callbackContext)
+    {
+        // .ReadValue<T>() gets the contained value from an input via a CallbackContext
+        direction = callbackContext.ReadValue<Vector2>();
+    }
+    #endregion
 }
--- Assets/NewInputSystem/SimpleInputSystem.cs
+++ Assets/NewInputSystem/SimpleInputSystem.cs
@@ -3,45 +3,63 @@ using UnityEngine.InputSystem;
 
 public class SimpleInputSystem : MonoBehaviour
 {
-    public float speed = 3, jumpPower = 10, gravity = 10;
+    public float speedWalk = 3, speedSprint = 7, jumpPower = 10, gravity = 10;
 
     private float ySpeed;
 
     CharacterController characterController;
 
-    InputAction jumpInput, moveInput;
+    // InputAction - represents an input from an Input Map.
+    // Can be pressed, held, released, or contain a value (e.g. Vector2)
+    InputAction jumpInput, moveInput, sprintInput;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+
+        // InputSystem.actions represents the project-wide InputActionAsset
+        // .FindAction() takes the name of the action (case sensitive)
         jumpInput = InputSystem.actions.FindAction("Jump");
         moveInput = InputSystem.actions.FindAction("Move");
+        sprintInput = InputSystem.actions.FindAction("Sprint");
     }
 
     void Update()
     {
+        // .ReadValue<T>() will attempt to read the InputAction's contained value
+        // You have to provide the correct type for the input
         Vector2 input = moveInput.ReadValue<Vector2>();
 
+        // Map the input to a 3D movement vector
         Vector3 movement = new(input.x, 0, input.y);
 
-        movement *= speed;
+        // Set a speed based on our sprint input
+        // .IsPressed() is like .GetButton()
+        movement *= sprintInput.IsPressed() ? speedSprint : speedWalk;
 
+        // Apply gravity to our y speed
         ySpeed -= gravity * Time.deltaTime;
 
+        // If the character controller is on the ground
         if (characterController.isGrounded)
         {
+            // Set ySpeed to -1 (slight downward pull)
             ySpeed = -1;
 
+            // .WasPressedThisFrame() is like .GetButtonDown()
             if (jumpInput.WasPressedThisFrame())
             {
                 ySpeed = jumpPower;
             }
         }
 
+        // Apply the ySpeed to our movement vector
         movement.y = ySpeed;
 
+        // Make movement happen based on time
         movement *= Time.deltaTime;
 
+        // Move the character controller
         characterController.Move(movement);
     }
 }
